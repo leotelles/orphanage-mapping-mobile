60b890b4569ed1f39cf93ebc0f7dbf455edc0f8c
Leonardo Telles <leo.c.telles@gmail.com>
2020-11-03 20:20:49 -0300

    Add Google map routes

--- src/pages/OrphanageDetails.tsx
+++ src/pages/OrphanageDetails.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Image, View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native';
+import { Image, View, ScrollView, Text, StyleSheet, Dimensions, Linking } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { Feather, FontAwesome } from '@expo/vector-icons';
-import { RectButton } from 'react-native-gesture-handler';
+import { RectButton, TouchableOpacity } from 'react-native-gesture-handler';
 import { useRoute } from '@react-navigation/native';
 
 import mapMarkerImg from '../images/map-marker.png';
@@ -48,6 +48,10 @@ export default function OrphanageDetails() {
     )
   }
 
+  function handleOpenGoogleMapsRoutes() {
+    Linking.openURL(`https://www.google.com/maps/dir/?api=1&destination=${orphanage?.latitude},${orphanage?.longitude}`);
+  }
+  
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imagesContainer}>
@@ -90,9 +94,9 @@ export default function OrphanageDetails() {
             />
           </MapView>
 
-          <View style={styles.routesContainer}>
+          <TouchableOpacity onPress={handleOpenGoogleMapsRoutes} style={styles.routesContainer}>
             <Text style={styles.routesText}>Ver rotas no Google Maps</Text>
-          </View>
+          </TouchableOpacity>
         </View>
       
         <View style={styles.separator} />
